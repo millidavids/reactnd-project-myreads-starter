4e36f76b37f174f8ab7bdfdb1a39f751bc38b492
millidavids <david.a.yurek@gmail.com>
2017-07-12 19:53:44 -0400

    feat: add the bookshelf component

---
+++ src/Bookshelf.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Book from './Book'
+
+const Bookshelf = (props) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.bookList.map((book, index) => {
+            return <li key={index}><Book data={book}/></li>
+          })}
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Bookshelf
